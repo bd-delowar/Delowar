ba660f6d21ac737d5ad2ed13302c1015a1ba1081
bd-delowar <delowarhossen199717@gamil.com>
2020-10-19 18:41:42 +0600

    initial commit

---
+++ tscript.js
@@ -0,0 +1,15 @@
+		function myFunction() {
+			 document.getElementById("demo").innerHTML = "Hellow World";
+		}
+		
+		function myDate() {
+			 document.getElementById("date").innerHTML = Date();
+		}
+
+
+		var firstName = "Delowar";
+		var lastName = " Hossen";
+		var fullName = firstName + lastName;
+
+
+		document.getElementById("name").innerHTML = "My first name is " + firstName + " and my last name is " + lastName + " and my full name is " + fullName;
